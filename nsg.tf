07082cffaa4a500024447e62abd58d4355b813b7
Ravi-94920 <134579420+Ravi-94920@users.noreply.github.com>
2024-07-15 18:44:35 +0530

    Add nsg.tf

---
+++ nsg.tf
@@ -0,0 +1,39 @@
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = "centralindia"
+  resource_group_name = "InvenicsDevOps-rg"
+
+  security_rule {
+    name                       = "allow-ssh"
+    description                 = "Allow SSH access"
+    access                     = "Allow"
+    direction                   = "Inbound"
+    priority                    = 100
+    protocol                    = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = "22"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+    source_address_prefix_list  = []
+    destination_address_prefix_list = []
+    source_port_ranges          = []
+    destination_port_ranges     = []
+  }
+
+  security_rule {
+    name                       = "allow-http"
+    description                 = "Allow HTTP access"
+    access                     = "Allow"
+    direction                   = "Inbound"
+    priority                    = 101
+    protocol                    = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = "80"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+    source_address_prefix_list  = []
+    destination_address_prefix_list = []
+    source_port_ranges          = []
+    destination_port_ranges     = []
+  }
+}
